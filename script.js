993dd9edbf017261773ad77ee7d340c106d1b462
nexylmao <vuletic.nenad@jjzmaj.edu.rs>
2018-12-30 14:26:18 +0100

    Updated script to automatically get token too

--- script.js
+++ script.js
@@ -1,45 +1,59 @@
 const WebSocketClient = require('ws');
+const axios = require('axios');
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-const HOST = 'HOST';
-const TOKEN = 'TOKEN';
+const HOST = 'localhost:8080';
 
-const ws = new WebSocketClient(HOST, {
+axios.post('http://' + HOST + '/authentication/login', {
+    identification: 'Nexyy',
+    password: 'Trackshittaz'
+}, {
     headers: {
-        token: TOKEN
+        'content-type': 'application/json'
     }
-});
+})
+    .then(response => {
+        const TOKEN = response.data.token;
+        const ws = new WebSocketClient('ws://' + HOST, {
+            headers: {
+                token: TOKEN
+            }
+        });
 
-ws.on('open', () => {
-    console.log('connected!');
+        ws.on('open', () => {
+            console.log('connected!');
 
-    ws.on('message', message => {
-        console.log('\nMESSAGE > ');
-        console.log(message);
-        console.log('\n');
-    });
-
-    ws.on('error', error => {
-        console.log('\nERROR > ');
-        console.error(error);
-        console.log('\n');
-    });
-
-    ws.on('close', () => {
-        console.log('connection closed.');
-    });
-
-    const ask = () => {
-        if (ws.readyState === ws.OPEN) {
-            rl.question('Enter message to send > ', json => {
-                ws.send(json);
-                ask();
+            ws.on('message', message => {
+                console.log('\nMESSAGE > ');
+                console.log(message);
+                console.log('\n');
             });
-        }
-    };
-    ask();
-});
+
+            ws.on('error', error => {
+                console.log('\nERROR > ');
+                console.error(error);
+                console.log('\n');
+            });
+
+            ws.on('close', () => {
+                console.log('connection closed.');
+            });
+
+            const ask = () => {
+                if (ws.readyState === ws.OPEN) {
+                    rl.question('Enter message to send > ', json => {
+                        ws.send(json);
+                        ask();
+                    });
+                }
+            };
+            ask();
+        });
+    })
+    .catch(err => {
+        console.error(err.response.data.message);
+    });
